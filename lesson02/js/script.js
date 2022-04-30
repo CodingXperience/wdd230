ab46f4d2a08c5bd5f1b4dbd22a0bc65ec05e7152
codingxperience <oko22002@byui.edu>
2022-04-30 20:06:44 +0300

    lesson02 assignment completed

---
+++ lesson02/js/script.js
@@ -0,0 +1,5 @@
+let my_date = new Date();
+let my_year = my_date.getFullYear();
+document.getElementById("the_year").textContent = my_year;
+
+document.getElementById("currentdate").textContent = document.lastModified
