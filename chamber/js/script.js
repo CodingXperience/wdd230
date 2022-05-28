f0a4bb73dc812fef3c057dbad85a0a592b8c9d7d
codingxperience <oko22002@byui.edu>
2022-05-29 00:31:55 +0300

    chamber week 6 added

--- chamber/js/script.js
+++ chamber/js/script.js
@@ -22,3 +22,32 @@ const fulldate = new Intl.DateTimeFormat("en-UK", {
 }).format(now);
 
 datefield.innerHTML = `<em>${fulldate}</em>`
+
+const today = new Date();
+console.log(today);
+
+const dayNumber = today.getDay();
+console.log(dayNumber);
+const element = document.getElementById("message");
+if (dayNumber == 6) {
+  element.classList.add("showme");
+}else {
+  element.classList.add("hideme");
+}
+
+const tempNumber = parseFloat(document.getElementById("temp").textContent);
+//console.log(tempNumber)
+const speedNumber = parseFloat(document.getElementById("speed").textContent);
+//console.log(speedNumber)
+
+let windchill = 35.74 + (0.6215 * tempNumber) - (35.75 * Math.pow(speedNumber,
+  0.16)) + (0.4275 * tempNumber * Math.pow(speedNumber, 0.16));
+  //console.log(windchill)
+  windchill = Math.round(windchill);
+  //console.log(windchill)
+
+  if (tempNumber<=50 && speedNumber > 3){
+    document.getElementById("chill").textContent = "Wind Chill: " + windchill + "\xB0F";
+  } else {
+    document.getElementById("chill").textContent = "N/A";
+  }
