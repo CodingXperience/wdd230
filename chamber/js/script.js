00eb7964754a152347e7259f19970e56f78db82f
codingxperience <oko22002@byui.edu>
2022-06-03 20:54:58 +0300

    lazyloading site complete

--- chamber/js/script.js
+++ chamber/js/script.js
@@ -27,7 +27,7 @@ const today = new Date();
 console.log(today);
 
 const dayNumber = today.getDay();
-console.log(dayNumber);
+//console.log(dayNumber);
 const element = document.getElementById("message");
 if (dayNumber == 1 || dayNumber == 2) {
   element.classList.add("showme");
