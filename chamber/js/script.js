f39923ab450095f8221d81148a25ee0739741139
codingxperience <oko22002@byui.edu>
2022-06-13 17:29:20 +0300

    sites refractored

--- chamber/js/script.js
+++ chamber/js/script.js
@@ -51,3 +51,27 @@ let windchill = 35.74 + (0.6215 * tempNumber) - (35.75 * Math.pow(speedNumber,
   } else {
     document.getElementById("chill").textContent = "N/A";
   }
+
+  // initialize display elements
+const todayDisplay = document.querySelector(".today");
+const visitsDisplay = document.querySelector(".visits");
+
+// get the stored value in localStorage
+let numVisits = Number(window.localStorage.getItem("visits-ls"));
+
+// determine if this is the first visit or display the number of visits.
+if (numVisits !== 0) {
+	visitsDisplay.textContent = numVisits;
+} else {
+	visitsDisplay.textContent = `This is your first visit!`;
+}
+
+// increment the number of visits.
+
+// how should this be improved?
+numVisits++;
+// store the new number of visits value
+localStorage.setItem("visits-ls", numVisits);
+
+// show todays date.
+todayDisplay.textContent = Date.now();
--- lesson07/js/scripts.js
+++ lesson07/js/scripts.js
@@ -1,67 +1,69 @@
+// get all images with data src attribute
+let imagesToLoad = document.querySelectorAll("img[data-src]");
+
+//optional parameters being set for the IntersectionalObserver
+//const imgOptions = {
+  //threshold: 1,
+  //rootMargin: "0px 0px 50px 0px"
+//};
+
+const loadImages = (image) => {
+  image.setAttribute("src", image.getAttribute("data-src"));
+  image.onload = () => {
+    image.removeAttribute("data-src");
+  };
+};
+
+// first check to see if Intersection Observer is supported
+if("IntersecionObserver" in window) {
+  const observer = new IntersectionObserver((items, observer) => {
+  items.forEach((item) => {
+    if(item.isIntersecting) {
+      loadImages(item.target);
+      observer.unobserve(item.target);
+    }
+  });
+});
+// loop through each img an check status and load if necessary
+imagesToLoad.forEach((img) => {
+  observer.observe(img);
+});
+}else {
+  imagesToLoad.forEach((img) => {
+   loadImages(img); 
+  });
+}
+
 // // get all images with data src attribute
-// const imagesToLoad = document.querySelectorAll("img[data-src]");
+// const images = document.querySelectorAll("[data-src]");
+
+// function preloadImage(img) {
+//   const src = img.getAttribute("data-src");
+//   if(!src) {
+//     return;
+//   }
+//   img.src = src;
+// }
 
 // //optional parameters being set for the IntersectionalObserver
 // const imgOptions = {
-//   threshold: 3,
+//   threshold: 1,
 //   rootMargin: "0px 0px 50px 0px"
 // };
 
-// const loadImages = (image) => {
-//   image.setAttribute("src", image.getAttribute("data-src"));
-//   image.onload = () => {image.removeAttribute("data-src")};
-// };
-
-// // first check to see if Intersection Observer is supported
-// if("IntersecionObserver" in window) {
-//   const imgObserver = new IntersectionObserver((items, observer) => {
-//   items.forEach((item) => {
-//     if(item.isIntersecting) {
-//       loadImages(item.target);
+// const imgObserver = new IntersectionObserver((entries, imgObserver) => {
+//     entries.forEach(entry => {
+//       if(!entry.isIntersecting) {
+//         return;
+//     }else {
+//       preloadImage(entry.target);
+//       imgObserver.unobserve(entry.target);
 //     }
-//   });
+//   })
 // }, imgOptions);
+
 // // loop through each img an check status and load if necessary
-// imagesToLoad.forEach((img) => {
-//   imgObserver.observe(img);
+// images.forEach(image => {
+//   imgObserver.observe(image);
 // });
-// }
-// else {
-//   imagesToLoad.forEach((img) => {
-//    loadImages(img); 
-//   });
-// }
-
-// get all images with data src attribute
-const images = document.querySelectorAll("[data-src]");
-
-function preloadImage(img) {
-  const src = img.getAttribute("data-src");
-  if(!src) {
-    return;
-  }
-  img.src = src;
-}
-
-//optional parameters being set for the IntersectionalObserver
-const imgOptions = {
-  threshold: 1,
-  rootMargin: "0px 0px 50px 0px"
-};
-
-const imgObserver = new IntersectionObserver((entries, imgObserver) => {
-    entries.forEach(entry => {
-      if(!entry.isIntersecting) {
-        return;
-    }else {
-      preloadImage(entry.target);
-      imgObserver.unobserve(entry.target);
-    }
-  })
-}, imgOptions);
-
-// loop through each img an check status and load if necessary
-images.forEach(image => {
-  imgObserver.observe(image);
-});
 
