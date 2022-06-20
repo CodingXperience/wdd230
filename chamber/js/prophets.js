80ae1e08f5249188ce7a1501c7e0b8306877b2e8
codingxperience <oko22002@byui.edu>
2022-06-20 19:32:54 +0300

    directory refractored

--- chamber/js/prophets.js
+++ chamber/js/prophets.js
@@ -24,6 +24,7 @@ function buildProphetCards(data) {
   data.companies.forEach(company => {
     let card = document.createElement("section");
     let name = document.createElement("h2");
+    let icon = document.createElement("img");
     let imageurl = document.createElement("p");
     let category = document.createElement("p");
     let weburl = document.createElement("p");
@@ -31,16 +32,20 @@ function buildProphetCards(data) {
     let phonenumber = document.createElement("p");
     let membershiplevel = document.createElement("p");
     let detail = document.createElement("p");
+    
 
 
     name.innerHTML = `${company.name}`;
+    icon.setAttribute("src", company.icon);
+    icon.setAttribute("alt", "membership level");
     //imageurl.innerHTML = `${prophet.imageurl}`;
     category.innerHTML = `Category: ${company.category}`;
     phonenumber.innerHTML = `Contact: ${company.phonenumber}`;
     membershiplevel.innerHTML = `${company.membershiplevel}`;
     state.innerHTML = `State: ${company.state}`;
-    weburl.innerHTML = `${company.weburl}`;
+    weburl.innerHTML = `<a href ="${company.weburl}" target="_blank">${company.name}'website link </a>`;
     detail.innerHTML = `${company.detail}`;
+    
     
 
     card.appendChild(name);
@@ -51,6 +56,7 @@ function buildProphetCards(data) {
     card.appendChild(state);
     card.appendChild(weburl);
     card.appendChild(detail);
+    card.appendChild(icon);
     cards.appendChild(card);
   })
 }
