967ae7a88c10203de1d3c728a4bf5539eb6182ba
codingxperience <oko22002@byui.edu>
2022-06-25 08:27:42 +0300

    site refactoring

--- chamber/js/script.js
+++ chamber/js/script.js
@@ -35,43 +35,61 @@ if (dayNumber == 1 || dayNumber == 2) {
   element.classList.add("hideme");
 }
 
-// const tempNumber = Number(document.getElementById("temp").textContent);
-// //console.log(tempNumber)
-// const speedNumber = Number(document.getElementById("speed").textContent);
-// //console.log(speedNumber)
 
-// let windchill = 35.74 + (0.6215 * tempNumber) - (35.75 * Math.pow(speedNumber,
-//   0.16)) + (0.4275 * tempNumber * Math.pow(speedNumber, 0.16));
-//   //console.log(windchill)
-//   windchill = Math.round(windchill);
-//   //console.log(windchill)
+  // initialize display elements
+const todayDisplay = document.querySelector(".today");
+const visitsDisplay = document.querySelector(".visits");
 
-//   if (tempNumber<=50 && speedNumber > 3){
-//     document.getElementById("chill").textContent = "Wind Chill: " + windchill + "\xB0F";
-//   } else {
-//     document.getElementById("chill").textContent = "N/A";
-//   }
+// get the stored value in localStorage
+let numVisits = Number(window.localStorage.getItem("visits-ls"));
 
-//   // initialize display elements
-// const todayDisplay = document.querySelector(".today");
-// const visitsDisplay = document.querySelector(".visits");
+// determine if this is the first visit or display the number of visits.
+if (numVisits !== 0) {
+	visitsDisplay.textContent = numVisits;
+} else {
+	visitsDisplay.textContent = `This is your first visit!`;
+}
 
-// // get the stored value in localStorage
-// let numVisits = Number(window.localStorage.getItem("visits-ls"));
+// increment the number of visits.
 
-// // determine if this is the first visit or display the number of visits.
-// if (numVisits !== 0) {
-// 	visitsDisplay.textContent = numVisits;
-// } else {
-// 	visitsDisplay.textContent = `This is your first visit!`;
-// }
+// how should this be improved?
+numVisits++;
+// store the new number of visits value
+localStorage.setItem("visits-ls", numVisits);
 
-// // increment the number of visits.
+// Using localStorage to display how many days it has been since the user's last visit to your site. 
 
-// // how should this be improved?
-// numVisits++;
-// // store the new number of visits value
-// localStorage.setItem("visits-ls", numVisits);
+// get the last visit date value stored in localStorage
+let lastVisitDate = window.localStorage.getItem("last-date-visit");
+console.log(lastVisitDate);
+console.log("Last visit was on:" + new Date(parseInt(lastVisitDate)));
+
+
+// get todays date - leave the variable todayy with double yy's its not an error,changing it it will cause errors
+let todayy = Date.now();
+
+//Using JS calculate how many milliseconds are in a day. 
+const msInDay = 1000 * 60 * 60 * 24;
+
+// Creating a variable for today and another variable for the last visit and then subtract and divide by the number of milliseconds. 
+// Then use Math.round() to make it a whole number.
+let difference = Math.round((todayy - lastVisitDate)/msInDay);
+console.log("Last visit difference in days is:" + parseInt(difference));
+
+//Displaying a message about how many days it's been or if this is the first visit.
+if (numVisits !== 0) {
+	//Displaying a message about how many days it's been
+	document.querySelector(".how-many-days-ago").textContent = 0;
+} else {
+	document.querySelector(".how-many-days-ago").textContent = difference;
+}
+
+// update the lastVisitDate
+localStorage.setItem("last-date-visit",Date.now());
+lastVisitDate = window.localStorage.getItem("last-date-visit");
+console.log(lastVisitDate);
+console.log("Last visit was on:" + new Date(parseInt(lastVisitDate)));
+
 
 
 
--- json_start1/js/weatherapi.js
+++ json_start1/js/weatherapi.js
@@ -5,3 +5,17 @@ const captionDesc = document.querySelector('figcaption');
 
 //ADD the key and change units to imperial
 const apiURL = "//api.openweathermap.org/data/2.5/weather?id=232422&appid=bb6b10f23ccbc81205ccce7971693fe4&units=imperial"
+
+fetch(apiURL)
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data); // this is temporary for development only
+    currentTemp.innerHTML = `<strong>${data.main.temp.toFixed(0)}</strong>`;
+    const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+    const desc = data.weather[0].description;
+
+    weatherIcon.setAttribute('src', iconsrc);weatherIcon.setAttribute('alt', desc);
+    captionDesc.textContent = desc;
+
+  });
+
