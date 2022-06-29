fbb55eee078a66162d28a9523a1df6cfa0b310c1
codingxperience <oko22002@byui.edu>
2022-06-29 16:42:57 +0300

    banner message fixed

--- chamber/js/script.js
+++ chamber/js/script.js
@@ -29,7 +29,7 @@ console.log(today);
 const dayNumber = today.getDay();
 //console.log(dayNumber);
 const element = document.getElementById("message");
-if (dayNumber == 1 || dayNumber == 6) {
+if (dayNumber == 1 || dayNumber == 3) {
   element.classList.add("showme");
 }else {
   element.classList.add("hideme");
