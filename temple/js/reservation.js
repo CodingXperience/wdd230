c8afdc21f1b184b8a9a15efa405bb230143d499b
codingxperience <oko22002@byui.edu>
2022-07-25 15:14:02 +0300

    weather place holder fixed

--- temple/js/reservation.js
+++ temple/js/reservation.js
@@ -4,7 +4,3 @@ function dateTime() {
 //store the time the page was loaded inside the form data
 document.querySelector("#DateTime").value =  dateTime();
 
-function adjustRating(quantity) {
-
-  document.getElementById("roomQuantity").innerHTML = quantity;
-}
